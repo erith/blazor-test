83cc712024bf87cfcfa29376f430519a840e3981
erith <erith9@gmail.com>
2022-12-14 21:00:17 +0900

    first init

---
+++ BlazorApp2/Client/Library/OzComponentBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace BlazorApp2.Client.Library
+{
+    public class OzComponentBase : ComponentBase, IDisposable
+    {
+        [Inject]
+        protected NavigationManager? nav { get; set; }
+        [Inject]
+        protected EventAggregator? eve { get; set; }
+
+        Subscription<NavigationContext>? sub;
+        protected override void OnInitialized()
+        {
+            if (eve != null)
+            {
+                sub = eve.Subscribe<NavigationContext>(OnNavigationChanged);
+            }
+
+            base.OnInitialized();
+        }
+
+        /// <summary>
+        /// Navigation이 바뀌었음을 대응합니다.
+        /// </summary>
+        /// <param name="args"></param>
+        protected virtual void OnNavigationChanged(NavigationContext args)
+        {
+
+        }
+
+        /// <summary>
+        /// Dispose 처리합니다.
+        /// </summary>
+        public virtual void Dispose()
+        {
+            if (eve != null && sub != null)
+                eve.UnSbscribe(sub);
+        }
+    }
+}
---
+++ BlazorApp2/Client/Library/PubSub.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+
+namespace BlazorApp2.Client.Library
+{
+    public class MessageArgument<T> : EventArgs
+    {
+        public T Message { get; set; }
+        public MessageArgument(T message)
+        {
+            Message = message;
+        }
+    }
+
+    public interface IPublisher<T>
+    {
+        event EventHandler<MessageArgument<T>> DataPublisher;
+        void OnDataPublisher(MessageArgument<T> args);
+        void PublishData(T data);
+    }
+
+    public class Publisher<T> : IPublisher<T>
+    {
+        //Defined datapublisher event  
+        public event EventHandler<MessageArgument<T>> DataPublisher;
+
+        public void OnDataPublisher(MessageArgument<T> args)
+        {
+            var handler = DataPublisher;
+            if (handler != null)
+                handler(this, args);
+        }
+
+
+        public void PublishData(T data)
+        {
+            MessageArgument<T> message = (MessageArgument<T>)Activator.CreateInstance(typeof(MessageArgument<T>), new object[] { data });
+            OnDataPublisher(message);
+        }
+    }
+
+    public class Subscriber<T>
+    {
+        public IPublisher<T> Publisher { get; private set; }
+        public Subscriber(IPublisher<T> publisher)
+        {
+            Publisher = publisher;
+        }
+    }
+
+    //Does used by EventAggregator to reserve subscription  
+    public class Subscription<Tmessage> : IDisposable
+    {
+        public Action<Tmessage> Action { get; private set; }
+        private readonly EventAggregator EventAggregator;
+        private bool isDisposed;
+        public Subscription(Action<Tmessage> action, EventAggregator eventAggregator)
+        {
+            Action = action;
+            EventAggregator = eventAggregator;
+        }
+
+        ~Subscription()
+        {
+            if (!isDisposed)
+                Dispose();
+        }
+
+        public void Dispose()
+        {
+            EventAggregator.UnSbscribe(this);
+            isDisposed = true;
+        }
+    }
+
+    public class EventAggregator
+    {
+        private Dictionary<Type, IList> subscriber;
+
+        public EventAggregator()
+        {
+            subscriber = new Dictionary<Type, IList>();
+        }
+
+        public void Publish<TMessageType>(TMessageType message)
+        {
+            Type t = typeof(TMessageType);
+            IList actionlst;
+            if (subscriber.ContainsKey(t))
+            {
+                actionlst = new List<Subscription<TMessageType>>(subscriber[t].Cast<Subscription<TMessageType>>());
+
+                foreach (Subscription<TMessageType> a in actionlst)
+                {
+                    a.Action(message);
+                }
+            }
+        }
+
+        public Subscription<TMessageType> Subscribe<TMessageType>(Action<TMessageType> action)
+        {
+            Type t = typeof(TMessageType);
+            IList actionlst;
+            var actiondetail = new Subscription<TMessageType>(action, this);
+
+            if (!subscriber.TryGetValue(t, out actionlst))
+            {
+                actionlst = new List<Subscription<TMessageType>>();
+                actionlst.Add(actiondetail);
+                subscriber.Add(t, actionlst);
+            }
+            else
+            {
+                actionlst.Add(actiondetail);
+            }
+
+            return actiondetail;
+        }
+
+        public void UnSbscribe<TMessageType>(Subscription<TMessageType> subscription)
+        {
+            Type t = typeof(TMessageType);
+            if (subscriber.ContainsKey(t))
+            {
+                subscriber[t].Remove(subscription);
+            }
+        }
+    }
+
+
+}
